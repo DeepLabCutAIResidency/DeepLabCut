0da56cefc932eb02795ad56d42035d55f5a658bd
sfmig <33267254+sfmig@users.noreply.github.com>
2022-07-21 18:47:28 +0200

    added comments

--- deeplabcut/data_augm_pipeline_scripts/notebook_convex_hull.py
+++ deeplabcut/data_augm_pipeline_scripts/notebook_convex_hull.py
@@ -14,15 +14,18 @@ def PolyArea(x,y):
 
 
 ########################################################
+### Set config path for the project and path to human labels
 config_path = '/media/data/stinkbugs-DLC-2022-07-15/config.yaml'
 cfg = auxiliaryfunctions.read_config(config_path)
 project_path = cfg["project_path"] # or: os.path.dirname(config_path) #dlc_models_path = os.path.join(project_path, "dlc-models")
 
+# ideally: next bit from params and config?----
 human_labels_filepath ='/media/data/stinkbugs-DLC-2022-07-15/data_augm_00_baseline/training-datasets/iteration-1/UnaugmentedDataSet_stinkbugsJul15/CollectedData_DLC.h5' #'/Users/user/Desktop/sabris-mouse/sabris-mouse-nirel-2022-07-06/training-datasets/iteration-0/UnaugmentedDataSet_sabris-mouseJul6/CollectedData_nirel.h5'
 df_human = pd.read_hdf(human_labels_filepath)
 df_human = df_human.droplevel('scorer',axis=1) #df_human['DLC'][:].iloc[0,:]
 
 ########################################################
+### Plot a selected image
 image_row_idx = 50
 img_relative_path = os.path.join(*df_human.index[image_row_idx])
 labeled_data_path = os.path.join(project_path, img_relative_path)
@@ -32,7 +35,8 @@ fig = plt.figure(figsize=(10,10))
 plt.imshow(image)
 
 ########################################################
-# df_human_wo_nan = df_human.dropna(axis=0).reset_index(drop=True)
+### Get keypoints for this image
+# df_human_wo_nan = df_human.dropna(axis=0).reset_index(drop=True) ----review! why it didnt work?
 lts = list(df_human.iloc[image_row_idx,:])
 x = lts[0::2]
 y = lts[1::2]
@@ -45,13 +49,16 @@ points = points.T
 plt.scatter(points[:,0],points[:,1])
 
 ########################################################
+### Compute convex hull 
 hull = ConvexHull(points)
 hull_indices = np.unique(hull.simplices.flat)
 hull_pts = points[hull_indices, :]
 
+# plot convex hull
 for simplex in hull.simplices:
     plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
 
+# plot vertices
 plt.scatter(hull_pts[:,0],
             hull_pts[:,1],
             color='r',s=50)
