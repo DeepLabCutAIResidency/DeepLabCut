bd5dc6a6721fdf57e2487c909c53d08690ef79a5
sfmig <33267254+sfmig@users.noreply.github.com>
2022-08-03 19:16:10 +0200

    notebook for selecting iframes in shuffles 2 and 3

--- deeplabcut/active_learning_iframes/notebook_active_learning_iframes.py
+++ deeplabcut/active_learning_iframes/notebook_active_learning_iframes.py
@@ -9,18 +9,47 @@ We need to
 
 # %%
 #####################################
+## Import deeplabcut
 import deeplabcut
+from deeplabcut.utils.auxiliaryfunctions import read_config, edit_config
+from deeplabcut.generate_training_dataset.trainingsetmanipulation import create_training_dataset
 
-
+import os
 
 
 # %%
 #######################################
 # Input params
 # - config, iteration=0, 50/50 split, shuffle=1 
-init_weights = \
-    '/home/sofia/datasets/Horses-Byron-2019-05-08/data_augm_00_baseline/dlc-models/iteration-0/HorsesMay8-trainset50shuffle1/train/snapshot-200000'
 
-######################################
+baseline_parent_dir = '/home/sofia/datasets/Horses-Byron-2019-05-08/data_augm_00_baseline'
+init_weights = \
+    os.path.join(baseline_parent_dir,'/dlc-models/iteration-0/HorsesMay8-trainset50shuffle1/train/snapshot-200000')
+
+
+#####################################################
+# Create training dataset with shufflles 2 and 3
+
+### Set config path
+config_path = '/home/sofia/datasets/Horses-Byron-2019-05-08/config.yaml' 
+
+
+### Data from shuffles 2 and 3
+labelled_data_shuffle_2 = \
+    os.path.join(baseline_parent_dir,
+                 '/training-datasets/iteration-0/UnaugmentedDataSet_HorsesMay8/Documentation_data-Horses_50shuffle2.pickle')
+
+labelled_data_shuffle_3 = \
+    os.path.join(baseline_parent_dir,
+                 '/training-datasets/iteration-0/UnaugmentedDataSet_HorsesMay8/Documentation_data-Horses_50shuffle3.pickle')
+
+## Set other params
+NUM_SHUFFLES=3
+create_training_dataset(
+    config_path,
+    num_shuffles=NUM_SHUFFLES) # augmenter_type=None, posecfg_template=None,
+
+###############################################
 # Create training dataset
-# based on i-frames from (shuffles? randomly sample a fraction of i-frames?)    
+# based on i-frames from (shuffles? randomly sample a fraction of i-frames?)   
+
