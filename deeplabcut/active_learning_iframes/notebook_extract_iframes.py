1a3c08c48788a425b07866b78638cf862e360a56
sfmig <33267254+sfmig@users.noreply.github.com>
2022-08-04 16:55:15 +0200

    plot for fraction of iframes per video and saving h5 file with iframe data

--- deeplabcut/active_learning_iframes/notebook_extract_iframes.py
+++ deeplabcut/active_learning_iframes/notebook_extract_iframes.py
@@ -57,7 +57,8 @@ for d in list_subdirs:
 # %% Extract i-frames per video
 # loop thru directories (one directory=one video)
 bool_iframes_in_df = [False]*len(df)
-
+list_step_btw_iframes = []
+map_subdirs_to_iframes_fraction = dict()
 for d in list(map_subdirs_to_files.keys()):
 
     # get image size (assuming common to all files in this directory)
@@ -80,23 +81,32 @@ for d in list(map_subdirs_to_files.keys()):
 
     # extract i-frames (idcs relative to video)
     idcs_i_frames_wrt_video = extract_i_frames(video_temp_path)
-    print(np.diff(np.asarray(idcs_i_frames_wrt_video)))
 
+    list_step_btw_iframes.append(np.diff(np.asarray(idcs_i_frames_wrt_video))) # print step between iframes
+    map_subdirs_to_iframes_fraction[d] = (len(idcs_i_frames_wrt_video),
+                                          len(map_subdirs_to_files[d]),
+                                          len(idcs_i_frames_wrt_video)/len(map_subdirs_to_files[d]))
+    print('I-frames in {}: {}/{} = {}'.format(d,
+                                            len(idcs_i_frames_wrt_video),
+                                            len(map_subdirs_to_files[d]),
+                                            len(idcs_i_frames_wrt_video)/len(map_subdirs_to_files[d])))# print fraction of iframes
+    
     # inspect how many frames from video are iframes
-    plt_bool_list_iframes = [True if j in idcs_i_frames_wrt_video else False for j in range(len(map_subdirs_to_files[d]))]
-    plt_bool_list_unif = [True if j%step_frames_unif==0 else False for j in range(len(map_subdirs_to_files[d]))]
-    # l=[i for i,x in enumerate(bool_list) if x]; l==idcs_i_frames_wrt_video --- True
-    plt.plot(plt_bool_list_iframes,'r.',label='iframes')
-    plt.plot(plt_bool_list_unif,'b.',label='unif')
-    plt.xlim([0,50])
-    plt.xlabel('frame')
-    plt.ylabel('i-frame == True')
-    plt.show()
-    # get frames filenames for i-frames
+    # plt_bool_list_iframes = [True if j in idcs_i_frames_wrt_video else False for j in range(len(map_subdirs_to_files[d]))]
+    # plt_bool_list_unif = [True if j%step_frames_unif==0 else False for j in range(len(map_subdirs_to_files[d]))]
+    # # l=[i for i,x in enumerate(bool_list) if x]; l==idcs_i_frames_wrt_video --- True
+    # plt.plot(plt_bool_list_iframes,'r.',label='iframes')
+    # plt.plot(plt_bool_list_unif,'b.',label='unif')
+    # plt.xlim([0,50])
+    # plt.xlabel('frame')
+    # plt.ylabel('i-frame == True')
+    # plt.show()
+
+    ## get frames filenames for i-frames
     # list_frames_png_str = map_subdirs_to_files[d]
     list_i_frames_png_str = [map_subdirs_to_files[d][j] for j in idcs_i_frames_wrt_video]
 
-    # add to dataframe
+    # add to boolean vector for dataframe
     bool_iframes_in_df = [x or y for x,y in zip(bool_iframes_in_df,
                                                 [True if el in list_i_frames_png_str else False 
                                                     for el in list_paths_to_files])] 
@@ -107,10 +117,26 @@ for d in list(map_subdirs_to_files.keys()):
     # idcs_where_true = [i for i,x in enumerate(bool_iframes_in_df) if x]
     # [list_paths_to_files[j] for j in idcs_where_true]==list_i_frames_png_str
 
-####################################################
-# add results to dataframe and save as h5
+######################################################
+# %%
+# Plot fraction of i-frames per video
+plt.plot(map_subdirs_to_iframes_fraction.keys(),
+        [w*100 for u,v,w in map_subdirs_to_iframes_fraction.values()],'.')
+plt.xticks(range(len(map_subdirs_to_iframes_fraction)), 
+            list(map_subdirs_to_iframes_fraction.keys()),
+            rotation = 45, ha="right", fontsize='x-small')
 
+plt.ylabel('fraction of i-frames (%)')
+plt.ylim([5,10])
+plt.show()
+####################################################
+# %%
+# add results to dataframe and 
 df.insert(0,"I-frame",bool_iframes_in_df)
 
+df.to_hdf(labelled_data_h5file.split('.h5')[0]+'_iframes.h5',
+          "df_with_missing",
+          format="table",
+          mode="w")
 #################################################
-# %% delete temp video if required (eventually user input)
+# %% delete temp video if required (eventually user input whether to keep it or not?)
