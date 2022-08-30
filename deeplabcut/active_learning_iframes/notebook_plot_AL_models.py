7008bd2000ae0ffa7cac306d5ddc70434f9e9af4
sfmig <33267254+sfmig@users.noreply.github.com>
2022-08-30 23:51:21 +0200

    plots for expts rd 2

--- deeplabcut/active_learning_iframes/notebook_plot_AL_models.py
+++ deeplabcut/active_learning_iframes/notebook_plot_AL_models.py
@@ -26,7 +26,8 @@ import pickle
 # %% Inputs
 
 parent_dir_path = '/home/sofia/datasets' #'/home/sofia/datasets/Horse10_AL_unif_OH'
-parent_dir_path_list = ['Horse10_AL_unif_OH','Horse10_AL_uncert_OH','Horse10_AL_infl_OH']
+parent_dir_path_list = ['Horse10_AL_unif_small_OH',#'Horse10_AL_unif_OH',
+                        'Horse10_AL_uncert_kmeans_OH','Horse10_AL_uncert_kmeans_rev_OH']
 model_prefix_list = [x[:-3] for x in parent_dir_path_list] #'Horse10_AL_infl' #'Horse10_AL_unif000'
 
 parent_dir_path_list = [os.path.join(parent_dir_path,x) for x in parent_dir_path_list]
@@ -39,22 +40,31 @@ NUM_SHUFFLES = 3
 # %%
 
 plt.figure(figsize=(7.5,5.5))
-list_md_fractions = np.arange(0,1.25,0.25)
+list_md_fractions = np.arange(0.,0.3,0.05) #np.arange(0,1.25,0.25)
 dict_md_colors = {k:v for k,v in zip(model_prefix_list,
                                     ['tab:blue','tab:green','tab:orange'])}
 list_shuffle_numbers = range(1,NUM_SHUFFLES+1)
+
+path_to_pickle_px_error_unif = '/home/sofia/datasets/Horse10_OH_outputs/Horse10_AL_unif_px_error.pkl'
+
 for (model_prefix,parent_dir) in zip(model_prefix_list,
                                      parent_dir_path_list):
     # get list of AL models within this strategy                                 
-    list_models_dirs = [x for x in os.listdir(parent_dir) if x.startswith(model_prefix) and not x.h('pkl')]  
+    list_models_dirs = [x for x in os.listdir(parent_dir) 
+                            if x.startswith(model_prefix) and not x.endswith('pkl')]  
     list_models_dirs.sort()
     # get normalised pixel error for the models in this strategy
-    path_to_pickle_px_error = os.path.join(parent_dir,
+    path_to_pickle_px_error = os.path.join('/home/sofia/datasets/Horse10_OH_outputs', #parent_dir,
                                            model_prefix+'_px_error.pkl')
     with open(path_to_pickle_px_error,'rb') as file:
-        [_, dict_norm_px_error_total_per_model] = pickle.load(file)
+        [_, dict_norm_px_error_total_per_model,_,_] = pickle.load(file)
 
-
+    #---------------------------------------- 
+    # if 'unif' in model_prefix:
+    #     with open(path_to_pickle_px_error_unif,'rb') as file:
+    #         [_, dict_norm_px_error_total_per_model2,_,_] = pickle.load(file)
+    #         dict_norm_px_error_total_per_model.update(dict_norm_px_error_total_per_model2)
+    #----------------------------------------
     # plot
     if model_prefix == 'Horse10_AL_unif':
         for sh in list_shuffle_numbers:
@@ -83,7 +93,7 @@ plt.show()
 
 plt.figure(figsize=(7.5,5.5))
 
-list_md_fractions = np.arange(0,1.25,0.25)
+list_md_fractions = np.arange(0.,0.3,0.05) #np.arange(0,1.25,0.25)
 dict_sh_colors = {k:v for k,v in zip(list_shuffle_numbers,
                                     ['tab:gray','tab:purple','tab:red'])}
 list_shuffle_numbers = range(1,NUM_SHUFFLES+1)
@@ -93,10 +103,10 @@ for (model_prefix,parent_dir) in zip(model_prefix_list,
     list_models_dirs = [x for x in os.listdir(parent_dir) if x.startswith(model_prefix) and not x.endswith('pkl')]  
     list_models_dirs.sort()
     # get normalised pixel error for the models in this strategy
-    path_to_pickle_px_error = os.path.join(parent_dir,
+    path_to_pickle_px_error = os.path.join('/home/sofia/datasets/Horse10_OH_outputs', #parent_dir,
                                            model_prefix+'_px_error.pkl')
     with open(path_to_pickle_px_error,'rb') as file:
-        [_, dict_norm_px_error_total_per_model] = pickle.load(file)
+        [_, dict_norm_px_error_total_per_model,_,_] = pickle.load(file)
 
 
     # plot
