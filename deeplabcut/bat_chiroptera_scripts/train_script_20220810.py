e35469fbfedf17d7015e2b0129a6bd21d49363e1
biol-jsh <scientistjonas@gmail..com>
2022-08-30 17:44:19 +0200

    recipe notebook

--- deeplabcut/bat_chiroptera_scripts/train_script_20220810.py
+++ deeplabcut/bat_chiroptera_scripts/train_script_20220810.py
@@ -3,7 +3,8 @@ import deeplabcut
 #config_path='/home/jonas2/DLC_files/projects/geneva_protocol_paper_austin_2020_bat_data-DLC-2022-08-03/config.yaml'
 config_path='/home/jonas2/DLC_files/projects/bat_augmentation_austin_2020_bat_data-DLC-2022-08-18/config.yaml'
 #model_prefix = 'data_augm_01_fliplr'
-model_prefix = 'data_augm_02_fliplr_rotate'
+#model_prefix = 'data_augm_02_fliplr_rotate'
+model_prefix = 'data_augm_01_fliplr'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 gputouse=3
 shuffles = [1,2,3,4]
